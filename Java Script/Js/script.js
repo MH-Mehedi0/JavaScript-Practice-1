15743b24d6cd3a133d9cb574677c28d34cb795e3
MH-Mehedi0 <144585649+MH-Mehedi0@users.noreply.github.com>
2023-10-17 13:27:20 +0600

    2nd commit

--- Java Script/Js/script.js
+++ Java Script/Js/script.js
@@ -1,50 +1,66 @@
 // var numbers = [12,24,31,64,52,76,34534];
 
 
-// // Find length of array ..........................
+// // // Find length of array ..........................
 
-// var numberslength=numbers.length;
-// console.log(numberslength);
+// // var numberslength=numbers.length;
+// // console.log(numberslength);
 
-// // Single item call...........................
+// // // Single item call...........................
 
-// console.log(numbers[4]);
+// // console.log(numbers[4]);
 
-// // Replace Element............................
+// // // Replace Element............................
 
-// numbers[2]=50;
-// console.log(numbers);
+// // numbers[2]=50;
+// // console.log(numbers);
 
-// // ....Find Index of an Element...................
+// // // ....Find Index of an Element...................
 
 // console.log(numbers.indexOf(64));
 
-// // Array Element push/ PoP ..................................
+// // // Array Element push/ PoP ..................................
 
-// numbers.push(33,34);
-// console.log(numbers);
+// // numbers.push(33,34);
+// // console.log(numbers);
 
-// // Array PoP .................................
+// // // Array PoP .................................
 
-// numbers.pop();
-// console.log(numbers);
+// // numbers.pop();
+// // console.log(numbers);
 
-// var number_of_family = 324;
-// var single_family_collection = 250;
+// // var number_of_family = 324;
+// // var single_family_collection = 250;
 
-// var total_collection = number_of_family * single_family_collection;
+// // var total_collection = number_of_family * single_family_collection;
 
-// console.log(total_collection);
+// // console.log(total_collection);
 
-// 02......................................
+// // 02......................................
 
-var quantity_of_apple = 17;
-var total_price = 391;
-var single_apple_price = total_price / quantity_of_apple;
+// var quantity_of_apple = 17;
+// var total_price = 391;
+// var single_apple_price = total_price / quantity_of_apple;
 
-console.log(single_apple_price);
+// console.log(single_apple_price);
 
 
+var price = 18;
+var discount = 25;
+var per_calculation = 100;
 
+var count = discount / per_calculation;
+var discount_price = price * count;
+
+console.log('You Will Pay '+ discount_price);
+
+
+var mainInput = 654;
+var percent = 27;
+var percent_value = percent/100;
+
+var result = mainInput * percent_value;
+
+console.log('Result is ' + result);
 
 
