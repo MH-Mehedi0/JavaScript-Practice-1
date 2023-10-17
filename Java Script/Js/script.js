c628c54ef8a29a00d1a61916d2bc7e05a4457ce3
MH-Mehedi0 <144585649+MH-Mehedi0@users.noreply.github.com>
2023-10-17 13:04:29 +0600

    Upload

---
+++ Java Script/Js/script.js
@@ -0,0 +1,50 @@
+// var numbers = [12,24,31,64,52,76,34534];
+
+
+// // Find length of array ..........................
+
+// var numberslength=numbers.length;
+// console.log(numberslength);
+
+// // Single item call...........................
+
+// console.log(numbers[4]);
+
+// // Replace Element............................
+
+// numbers[2]=50;
+// console.log(numbers);
+
+// // ....Find Index of an Element...................
+
+// console.log(numbers.indexOf(64));
+
+// // Array Element push/ PoP ..................................
+
+// numbers.push(33,34);
+// console.log(numbers);
+
+// // Array PoP .................................
+
+// numbers.pop();
+// console.log(numbers);
+
+// var number_of_family = 324;
+// var single_family_collection = 250;
+
+// var total_collection = number_of_family * single_family_collection;
+
+// console.log(total_collection);
+
+// 02......................................
+
+var quantity_of_apple = 17;
+var total_price = 391;
+var single_apple_price = total_price / quantity_of_apple;
+
+console.log(single_apple_price);
+
+
+
+
+
