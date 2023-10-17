95d3859ef16cd32cdfa603b42b70b73e4b19c5ea
MH-Mehedi0 <144585649+MH-Mehedi0@users.noreply.github.com>
2023-10-17 22:31:22 +0600

    Add files via upload

---
+++ JS class 1/script.js
@@ -0,0 +1,66 @@
+// Mina Rina .............................
+
+var mina_rina_have = 7532;
+var mina_have_extra =560;
+var money_to_share = mina_rina_have - mina_have_extra;
+var rina_will_get = money_to_share / 2;
+var mina_will_get = rina_will_get + mina_have_extra;
+
+console.log( 'Rina will get '+ rina_will_get + 'Taka');
+console.log('Mina will get '+ mina_will_get + 'Taka');
+
+// Father daughter.........................
+
+var father_daughter_age = 80;
+var estimated_age_of_daughter = 1;
+var estimated_age_of_father = 4;
+var total_estimated_age = 5;
+
+var daughter_age = father_daughter_age / total_estimated_age;
+var father_age = daughter_age * 4;
+
+console.log('Daughter Age ' + daughter_age);
+console.log('Father Age ' + father_age);
+
+// Egg ........................................
+
+var given_eggs_total_price = 72;
+var number_of_eggs = 8;
+var need_eggs = 15;
+var single_egg_price = given_eggs_total_price / number_of_eggs;
+var total_egg_price = single_egg_price * need_eggs;
+
+console.log('Fifteen egg price is '+ total_egg_price);
+
+
+// Mas Bebsa .................................................
+
+var given_taka = 3000;
+var prawn_per_kg = 480;
+var shing_per_kg = 550;
+var need_prawn = 3;
+var need_shing = 2;
+
+var prawn_fish_price = prawn_per_kg * need_prawn;
+var shing_fish_price = shing_per_kg * need_shing;
+var total_price = prawn_fish_price + shing_fish_price;
+var get_money_back = given_taka - total_price;
+
+console.log('Get Money Back ' + get_money_back);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
